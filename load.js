bdf3459ca026c6ab52a2c40034e67c3b16c3c611
N0Ls <44846959+N0Ls@users.noreply.github.com>
2018-11-07 19:52:05 +0100

    Upload Main Files

---
+++ load.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+
+const bot = new Discord.Client();
+const config = require("./config.json");
+
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+bot.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    bot.on(eventName, event.bind(null, bot));
+  });
+});
+
+bot.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    bot.commands.set(commandName, props);
+  });
+});
+
+bot.login(config.token);
