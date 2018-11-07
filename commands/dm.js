27f6d0cd4aadf5b19a4ea241f9e682a648e4e5eb
N0Ls <44846959+N0Ls@users.noreply.github.com>
2018-11-07 21:34:07 +0100

    Adding comments

--- commands/dm.js
+++ commands/dm.js
@@ -1,7 +1,11 @@
 exports.run = (bot, message, args) => {
-	const guild=bot.guilds.get("487599932463775785");
+	/*DM the guild owner*/
+	const guild=bot.guilds.get("YOUR GUILD ID");
 	const sayMessage = args.join(" ");
 	
+	/*Sends the username of the author*/
+	/*Send the message itself*/
+	/*Delete the message from channel*/
 	guild.owner.send(message.author.username);
 	guild.owner.send(sayMessage);
 	message.delete().catch(O_o=>{});
